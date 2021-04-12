d66011dc9438e704f2e78a51640c41df3806f7d8
Dado83 <83deadpool@gmail.com>
2021-04-12 20:53:40 +0200

    Add description text to About component

--- src/App/About.js
+++ src/App/About.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import FPAbout from '../components/About/FPabout'
 
 export default function About(props) {
 
   return (
-    <h1>Nema</h1>
+    <FPAbout />
   )
 }
---
+++ src/components/About/FPabout.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function FPAbout(props) {
+
+    return (
+        <article>
+            <h1>Fair Play Liga Budućih Šampiona</h1>
+            <img src="" alt="grb" />
+            <p>Fair Play Liga je formirana je 2014. god. sa osnovnim ciljem da podstakne najmlađe učesnike sportskih manfestacija – djecu,
+            da se kroz igru i zabavu nauče fer-plej ponašanju.
+            Osnovna ideja u osnivanju Fair Play lige je bila da se propišu jedinstveni standardi –
+            propozicije i pravila koja su potpuno prilagođena djeci.</p>
+            <p><i>OSNOVNI PRINCIPI U ORGANIZACIJI FAIR PLAY LIGE</i></p>
+            <p> Fair Play Liga se sprovodi na osnovu jedinstvenog sistema takmičenja, koji je potpuno prilagođen djeci,
+            zavisno od njihovog uzrasta:</p>
+            <p>– ravnopravne takmičarske grupe, svako dijete se može radovati po nekoj pobjedi čak i početnici,
+            pa se ne gubi motiv za bavljenjem sportom,</p>
+            <p> – 3. poluvrijeme svih utakmica, da bi djeca koja su manje igrala u regularnom toku dobila šansu da pokažu svoje
+            umijeće,</p>
+            <p>– djevojčice i dječaci u istim timovima, afirmacija ideje da se sport ne dijeli na muški i ženski.</p>
+        </article>
+    )
+}
+
