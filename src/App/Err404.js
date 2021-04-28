cfb0d8b3617fc69516271648d059cc6c25227718
Dado83 <83deadpool@gmail.com>
2021-04-28 20:18:25 +0200

    Add Loader component

--- src/App/Err404.js
+++ src/App/Err404.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export default function Err404(props) {
+export default function Err404() {
 
   return (
     <>
