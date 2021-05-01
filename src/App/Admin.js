2e75ab150e7bc3673d2953b0c5f3188488d13723
Dado83 <83deadpool@gmail.com>
2021-05-01 21:04:09 +0200

    Tweak navbar

--- src/App/Admin.js
+++ src/App/Admin.js
@@ -3,10 +3,28 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 
 export default function Admin(props) {
+  const [php, setphp] = useState('sss');
+
+  useEffect(() => {
+    fetch('http://localhost/db.php')
+      .then(res => {
+        console.log(res);
+        console.log(php);
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        console.log(php);
+        setphp(data);
+        console.log(php);
+      })
+      .catch(err => console.log(err))
+  })
 
   return (
     <div className='content'>
       <h1>admin</h1>
+      <p>{php}</p>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import './styles.css';
+import logo from '../../assets/logo/grb.png';
 
 export default function Navbar() {
     return (
         <ul className='navbar'>
+            <img src={logo} />
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/raspored'>Raspored</Link></li>
             <li><Link to='/rezultati'>Rezultati</Link></li>
