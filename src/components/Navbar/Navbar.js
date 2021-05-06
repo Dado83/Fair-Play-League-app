2b2374013e162384f895eb70b67f5126df083c78
Dado83 <83deadpool@gmail.com>
2021-05-06 18:37:06 +0200

    Use logo as home link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,14 +5,13 @@ import logo from '../../assets/logo/grb.png';
 export default function Navbar() {
     return (
         <ul className='navbar'>
-            <img src={logo} />
-            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/'><img src={logo} /></Link></li>
             <li><Link to='/raspored'>Raspored</Link></li>
             <li><Link to='/rezultati'>Rezultati</Link></li>
             <li><Link to='/klubovi'>Klubovi</Link></li>
             <li><Link to='/o-nama'>O nama</Link></li>
             <li><Link to='/admin'>Admin</Link></li>
-            <li><Link to='/napravi-raspored'>Turnir kreator</Link></li>
+            <li><Link to='/napravi-raspored'>Turnir</Link></li>
         </ul>
     );
 }
