7e8dfd1cec4979987096ca765f12e2bfe8743e21
Dado83 <83deadpool@gmail.com>
2021-05-07 17:49:08 +0200

    Use rem units

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -21,41 +21,39 @@ export default function Result(props) {
     }, [props.mDay]);
 
     return (
-        <>
-            <table>
-                <thead>
-                    <tr>
-                        <th>{props.mDay}. kolo</th>
+        <table>
+            <thead>
+                <tr>
+                    <th>{props.mDay}. kolo</th>
+                </tr>
+                <tr>
+                    <th>domacin</th>
+                    <th>gost</th>
+                    <th colSpan='2'>2007</th>
+                    <th colSpan='2'>2008</th>
+                    <th colSpan='2'>2009</th>
+                    <th colSpan='2'>2010</th>
+                    <th colSpan='2'>2011</th>
+                </tr>
+            </thead>
+            <tbody>
+                {result.map(res => (
+                    <tr key={res.id}>
+                        <td>{res.home_name}</td>
+                        <td>{res.away_name}</td>
+                        <td>{res.goals_home7}</td>
+                        <td>{res.goals_away7}</td>
+                        <td>{res.goals_home8}</td>
+                        <td>{res.goals_away8}</td>
+                        <td>{res.goals_home9}</td>
+                        <td>{res.goals_away9}</td>
+                        <td>{res.goals_home10}</td>
+                        <td>{res.goals_away10}</td>
+                        <td>{res.goals_home11}</td>
+                        <td>{res.goals_away11}</td>
                     </tr>
-                    <tr>
-                        <th>domacin</th>
-                        <th>gost</th>
-                        <th colSpan='2'>2007</th>
-                        <th colSpan='2'>2008</th>
-                        <th colSpan='2'>2009</th>
-                        <th colSpan='2'>2010</th>
-                        <th colSpan='2'>2011</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {result.map(res => (
-                        <tr key={res.id}>
-                            <td>{res.home_name}</td>
-                            <td>{res.away_name}</td>
-                            <td>{res.goals_home7}</td>
-                            <td>{res.goals_away7}</td>
-                            <td>{res.goals_home8}</td>
-                            <td>{res.goals_away8}</td>
-                            <td>{res.goals_home9}</td>
-                            <td>{res.goals_away9}</td>
-                            <td>{res.goals_home10}</td>
-                            <td>{res.goals_away10}</td>
-                            <td>{res.goals_home11}</td>
-                            <td>{res.goals_away11}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </>
+                ))}
+            </tbody>
+        </table>
     );
 }
