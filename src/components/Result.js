7f3115ff82972d5471b93f31f2178fca6291a5fc
Dado83 <83deadpool@gmail.com>
2021-06-16 21:01:44 +0200

    Add logos

--- src/components/Result.js
+++ src/components/Result.js
@@ -23,33 +23,29 @@ export default function Result(props) {
         <table className='result'>
             <thead>
                 <tr>
-                    <th colSpan='12'>{props.mDay}. kolo</th>
-                </tr>
-                <tr>
-                    <th>domacin</th>
-                    <th>gost</th>
-                    <th colSpan='2' className={props.gen7}>2007</th>
-                    <th colSpan='2' className={props.gen8}>2008</th>
-                    <th colSpan='2' className={props.gen9}>2009</th>
-                    <th colSpan='2' className={props.gen10}>2010</th>
-                    <th colSpan='2' className={props.gen11}>2011</th>
+                    <th colSpan='5'>{props.mDay}. kolo</th>
                 </tr>
             </thead>
             <tbody>
                 {result.map(res => (
                     <tr key={res.id}>
-                        <td>{res.home_name}</td>
-                        <td>{res.away_name}</td>
+                        <td>{res.home_name}<img src={`http://localhost/api/logos/${res.home_id}.png`} /></td>
                         <td className={props.gen7}>{res.goals_home7}</td>
+                        <td className={props.gen7}>-</td>
                         <td className={props.gen7}>{res.goals_away7}</td>
                         <td className={props.gen8}>{res.goals_home8}</td>
+                        <td className={props.gen8}>-</td>
                         <td className={props.gen8}>{res.goals_away8}</td>
                         <td className={props.gen9}>{res.goals_home9}</td>
+                        <td className={props.gen9}>-</td>
                         <td className={props.gen9}>{res.goals_away9}</td>
                         <td className={props.gen10}>{res.goals_home10}</td>
+                        <td className={props.gen10}>-</td>
                         <td className={props.gen10}>{res.goals_away10}</td>
                         <td className={props.gen11}>{res.goals_home11}</td>
+                        <td className={props.gen11}>-</td>
                         <td className={props.gen11}>{res.goals_away11}</td>
+                        <td><img src={`http://localhost/api/logos/${res.away_id}.png`} />{res.away_name}</td>
                     </tr>
                 ))}
             </tbody>
