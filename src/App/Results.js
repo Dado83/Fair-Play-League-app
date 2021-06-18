20c64fbc9fe599dcb131047e3ade04410d510c25
Dado83 <83deadpool@gmail.com>
2021-06-18 22:06:48 +0200

    Add separate results page component

--- src/App/Results.js
+++ src/App/Results.js
@@ -1,10 +1,10 @@
-import Result from '../components/Result';
+import ResultPage from '../components/ResultPage';
 
 
 export default function Results() {
   let results = [];
   for (let i = 1; i <= 11; i++) {
-    results[i] = <Result key={i} mDay={i} />;
+    results[i] = <ResultPage key={i} mDay={i} />;
   }
 
   return (
