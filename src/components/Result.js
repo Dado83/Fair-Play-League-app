71ec3b0ff9331fae0df72a6925c4315e02635251
Dado83 <83deadpool@gmail.com>
2021-06-18 20:22:43 +0200

    Add youth selection header&asterix in results

--- src/components/Result.js
+++ src/components/Result.js
@@ -23,28 +23,34 @@ export default function Result(props) {
         <table className='result'>
             <thead>
                 <tr>
-                    <th colSpan='5'>{props.mDay}. kolo</th>
+                    <th>{props.mDay}. kolo</th>
+                    <th colSpan='3' className={props.gen7}>2007</th>
+                    <th colSpan='3' className={props.gen8}>2008</th>
+                    <th colSpan='3' className={props.gen9}>2009</th>
+                    <th colSpan='3' className={props.gen10}>2010</th>
+                    <th colSpan='3' className={props.gen11}>2011</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                 {result.map(res => (
                     <tr key={res.id}>
                         <td>{res.home_name}<img src={`http://localhost/api/logos/${res.home_id}.png`} /></td>
-                        <td className={props.gen7}>{res.goals_home7}</td>
+                        <td className={props.gen7}>{res.goals_home7 != -1 ? res.goals_home7 : '*'}</td>
                         <td className={props.gen7}>-</td>
-                        <td className={props.gen7}>{res.goals_away7}</td>
+                        <td className={props.gen7}>{res.goals_away7 != -1 ? res.goals_away7 : '*'}</td>
                         <td className={props.gen8}>{res.goals_home8}</td>
                         <td className={props.gen8}>-</td>
                         <td className={props.gen8}>{res.goals_away8}</td>
                         <td className={props.gen9}>{res.goals_home9}</td>
                         <td className={props.gen9}>-</td>
                         <td className={props.gen9}>{res.goals_away9}</td>
-                        <td className={props.gen10}>{res.goals_home10}</td>
+                        <td className={props.gen10}>{res.goals_home10 != -1 ? res.goals_home10 : '*'}</td>
                         <td className={props.gen10}>-</td>
-                        <td className={props.gen10}>{res.goals_away10}</td>
-                        <td className={props.gen11}>{res.goals_home11}</td>
+                        <td className={props.gen10}>{res.goals_away10 != -1 ? res.goals_away10 : '*'}</td>
+                        <td className={props.gen11}>{res.goals_home11 != -1 ? res.goals_home11 : '*'}</td>
                         <td className={props.gen11}>-</td>
-                        <td className={props.gen11}>{res.goals_away11}</td>
+                        <td className={props.gen11}>{res.goals_away11 != -1 ? res.goals_away11 : '*'}</td>
                         <td><img src={`http://localhost/api/logos/${res.away_id}.png`} />{res.away_name}</td>
                     </tr>
                 ))}
