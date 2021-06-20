0d64380afaa06089b60bdeba219adc6348f233dc
Dado83 <83deadpool@gmail.com>
2021-06-20 18:06:22 +0200

    Add archive component

---
+++ src/components/Archive.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Archive(props) {
+    const [archive, setArchive] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost/api/archive.php?year=${props.year}`)
+            .then(response => response.json())
+            .then(data => setArchive(data))
+            .catch(err => console.log(err))
+    }, [props.year]);
+
+    return (
+        <table className='archive'>
+            <tr>
+                <td colSpan='2'>#</td>
+                <td>1.mjesto</td>
+                <td>2. mjesto</td>
+                <td>3. mjesto</td>
+                <td>4. mjesto</td>
+                <td>naj kolektiv</td>
+            </tr>
+            {archive.map((arc, index) => (
+                <tr>
+                    <td>{++index}</td>
+                    <td>{arc.team}</td>
+                    <td>{arc.first}</td>
+                    <td>{arc.second}</td>
+                    <td>{arc.third}</td>
+                    <td>{arc.fourth}</td>
+                    <td>{arc.league_best_team ? 'da' : ''}</td>
+                </tr>
+            ))}
+        </table>
+    );
+}
+
