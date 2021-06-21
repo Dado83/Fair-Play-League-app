3a390cebf701757e9b8f3616a2571a075962a67f
Dado83 <83deadpool@gmail.com>
2021-06-21 20:45:56 +0200

    Change column in archive table

--- src/components/Archive.js
+++ src/components/Archive.js
@@ -11,28 +11,32 @@ export default function Archive(props) {
     }, [props.year]);
 
     return (
-        <table className='archive'>
-            <tr>
-                <td>#</td>
-                <td></td>
-                <td>1.mjesto</td>
-                <td>2. mjesto</td>
-                <td>3. mjesto</td>
-                <td>4. mjesto</td>
-                <td>naj kolektiv</td>
-            </tr>
-            {archive.map((arc, index) => (
+        <>
+            <table className='archive'>
                 <tr>
-                    <td>{++index}</td>
-                    <td>{arc.team}</td>
-                    <td>{arc.first}</td>
-                    <td>{arc.second}</td>
-                    <td>{arc.third}</td>
-                    <td>{arc.fourth}</td>
-                    <td>{arc.league_best_team ? 'da' : ''}</td>
+                    <td>#</td>
+                    <td></td>
+                    <td>1.mjesto</td>
+                    <td>2. mjesto</td>
+                    <td>3. mjesto</td>
+                    <td>4. mjesto</td>
+                    <td>bod</td>
                 </tr>
-            ))}
-        </table>
+                {archive.map((arc, index) => (
+                    <tr>
+                        <td>{++index}</td>
+                        <td>{arc.team}</td>
+                        <td>{arc.first}</td>
+                        <td>{arc.second}</td>
+                        <td>{arc.third}</td>
+                        <td>{arc.fourth}</td>
+                        <td>{arc.points}</td>
+                    </tr>
+                ))}
+            </table>
+            <p>legenda ove ikonice...</p>
+            <p>all time tabela</p>
+        </>
     );
 }
 
