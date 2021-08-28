28cb94a100700d3d38288775d34bfdb1a77a9dca
Dado83 <83deadpool@gmail.com>
2021-08-28 16:56:30 +0200

    Get visitor&hit count

--- src/App/Admin.js
+++ src/App/Admin.js
@@ -6,10 +6,36 @@ import Login from '../components/Login';
 export default function Admin(props) {
   const site = document.location.hostname;
   const [user, setUser] = useState('');
+  const [visits, setVisits] = useState('');
+  const [hits, setHits] = useState('');
+  const [lastDay, setLastDay] = useState('');
+  const [last2hrs, setLast2hrs] = useState('');
 
   const url = window.location.href;
   useEffect(() => {
     fetch(`http://localhost/api/visitors.php?counter=${url}`);
+  }, []);
+
+  useEffect(() => {
+    fetch(`http://localhost/api/visitors.php?counter=getvisits`)
+      .then(response => response.json())
+      .then(data => setVisits(prevState => data))
+      .catch(err => console.log(err));
+
+    fetch(`http://localhost/api/visitors.php?counter=gethits`)
+      .then(response => response.json())
+      .then(data => setHits(prevState => data))
+      .catch(err => console.log(err));
+
+    fetch(`http://localhost/api/visitors.php?counter=getday`)
+      .then(response => response.json())
+      .then(data => setLastDay(prevState => data))
+      .catch(err => console.log(err));
+
+    fetch(`http://localhost/api/visitors.php?counter=get2hrs`)
+      .then(response => response.json())
+      .then(data => setLast2hrs(prevState => data))
+      .catch(err => console.log(err));
   }, []);
 
   const handleChange = (param) => {
@@ -31,6 +57,10 @@ export default function Admin(props) {
         <button style={{ float: 'right' }} onClick={() => {
           setUser(user => sessionStorage.removeItem('role'))
         }}>logout</button></h2>
+      <p>Posjete: {visits}</p>
+      <p>Prethodni dan:{lastDay}</p>
+      <p>Zadnja 2 sata:{last2hrs}</p>
+      <p>Pregledi:{hits}</p>
       <div className='content'>
         <Dashboard />
       </div>
