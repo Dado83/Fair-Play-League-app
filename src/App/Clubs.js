679640a93e64c12b13083ae876520bffdc102efe
Dado83 <83deadpool@gmail.com>
2021-10-31 19:06:14 +0100

    Update club loop

--- src/App/Clubs.js
+++ src/App/Clubs.js
@@ -11,7 +11,7 @@ export default function Clubs() {
   }, []);
 
   let clubs = [];
-  for (let i = 1; i <= 11; i++) {
+  for (let i = 1; i <= 8; i++) {
     clubs[i] = <Club key={i} id={i} site={site} />;
   }
 
