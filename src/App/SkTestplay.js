d5c4ddfffdf3a3d1968b020e22e791c494c1d422
Dado83 <83deadpool@gmail.com>
2022-01-01 21:24:02 +0100

    Use visitor counter code

--- src/App/SkTestplay.js
+++ src/App/SkTestplay.js
@@ -4,10 +4,17 @@ import { sk } from '../assets/sktestplay/sk';
 import SkSearch from '../components/SkTestP/SkSearch';
 import SkGames from '../components/SkTestP/SkGames';
 import '../assets/stylesSk.css';
+import { protocol } from '../utility/utility';
 
 export default function SkTestplay(props) {
   const [games, setGames] = useState([]);
   const [isTempList, setTempList] = useState(false);
+  const site = document.location.hostname;
+  const url = window.location.href;
+
+  useEffect(() => {
+    fetch(`${protocol}://${site}/api/visitors.php?counter=${url}`);
+  }, []);
 
   useEffect(() => {
     setGames(skTemp);
