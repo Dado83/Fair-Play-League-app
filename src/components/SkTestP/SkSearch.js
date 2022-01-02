0e57f412c4a3705f50971ab6c11225a78aba0d0e
Dado83 <83deadpool@gmail.com>
2022-01-02 20:55:18 +0100

    Fix import

--- src/components/SkTestP/SkSearch.js
+++ src/components/SkTestP/SkSearch.js
@@ -1,4 +1,4 @@
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import { sk } from '../../assets/sktestplay/sk';
 
 export default function SkSearch(props) {
