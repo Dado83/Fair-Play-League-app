d26a3dffe9e9e3d4207920ed61a7ff3f7c3e3716
Dado83 <83deadpool@gmail.com>
2022-01-17 17:28:05 +0100

    Render array of results

--- src/App/Results.js
+++ src/App/Results.js
@@ -6,17 +6,28 @@ export default function Results() {
   const site = document.location.hostname;
   const url = window.location.href;
   const [results, setResults] = useState([]);
+  const [mDayNum, setMDN] = useState('');
 
   useEffect(() => {
     fetch(`${protocol}://${site}/api/visitors.php?counter=${url}`);
   }, []);
 
   useEffect(() => {
-    let res = [];
-    for (let i = 1; i <= 7; i++) {
-      res[i] = <ResultPage key={i} mDay={i} site={site} />;
-    }
-    setResults(res);
+    let md;
+    const getMday = async () => {
+      const mdayReq = await fetch(`${protocol}://${site}/api/results.php?nextfix=nextfix`);
+      console.log('fetch ', mdayReq);
+      const mday = await mdayReq.json();
+      console.log('json ', mday);
+      md = mday - 1;
+
+      let res = [];
+      for (let i = 1; i <= md; i++) {
+        res[i] = <ResultPage key={i} mDay={i} site={site} />;
+      }
+      setResults(res);
+    };
+    getMday();
   }, []);
 
   return <div className='content'>{results.map((res) => res)}</div>;
