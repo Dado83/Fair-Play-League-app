df3fbf904076b2fa700872570fef773012130ff1
Dado83 <83deadpool@gmail.com>
2022-02-20 20:07:19 +0100

    Minimize info

--- src/components/Finals.js
+++ src/components/Finals.js
@@ -1,15 +1,7 @@
-import React, { useState, useEffect } from 'react';
-
 export default function Finals(props) {
   return (
-    <>
-      <div className='finals'>
-        Završni turnir Lige je na rasporedu 26. i 27. februara 2022.
-        <div>{props.team1}</div>
-        <div>{props.team2}</div>
-        <div>{props.team3}</div>
-        <div>{props.team4}</div>
-      </div>
-    </>
+    <div className='finals'>
+      <p>Završni turnir Lige je na rasporedu 26. i 27. februara 2022. u Gradačcu</p>
+    </div>
   );
 }
